57136ac3aac3de127c25fc510c03d6463fa43523
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-03-28 16:58:00 -0300

    req 1: implementa model e migration de 'teams'

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,27 @@
+import { STRING, INTEGER, Model } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams', // recebe o nome da tabela
+  timestamps: false,
+});
+
+export default Teams;
